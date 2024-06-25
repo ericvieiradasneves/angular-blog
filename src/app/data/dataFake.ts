b13fe94b273f8b40998356ac655019eae117ae81
Eric vieira <eric.vieira@trocados.com.br>
2024-06-25 10:45:25 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "A Casa do Dragão mostra que conflitos internos são tão perigosos quanto dragões",
+    "description": "Série exibiu o segundo episódio neste domingo (23)",
+    "photoCover": "https://cdn.ome.lt/aA4qiuBRmedlM8m6p7zqQaH_0es=/480x360/smart/extras/conteudos/ali_TWVQgyB.png"
+  },
+  {
+    "id":"2",
+    "title": "The Batman - Parte II | Ator revela quando começarão as filmagens do filme",
+    "description": "Poucos detalhes do longa foram revelados",
+    "photoCover": "https://cdn.ome.lt/hJRCBNEGUvZGlvIgVN5bo6G_hAE=/987x0/smart/uploads/conteudo/fotos/bat_VKduPZn_y6GeqYM.jpeg"
+  }
+]
